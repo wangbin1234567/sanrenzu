8a2c58c39b02a2e9c811d9d653a5a4c56f589038
wangbin1234567 <you@2930134036.com>
2019-12-08 21:25:59 +0800

    'last-2'

---
+++ dist/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+import VueLazyload from "vue-lazyload"
+import "lib-flexible/flexible.js"
+Vue.config.productionTip = false
+// Vue.use(VueLazyload)
+import Vant from "vant"
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+
+//配置图片懒加载
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=984435346,506206561&fm=15&gp=0.jpg',
+  loading: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=984435346,506206561&fm=15&gp=0.jpg',
+  attempt: 1
+})
+
+
+new Vue({
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ dist/src/views/img/index.vue
@@ -0,0 +1,91 @@
+
+<template>
+    <div class="series_wrap">
+
+        <header class="series_wrap_header">
+            <p @click="seriescolor">颜色<span></span></p>
+            <p @click="seriestype">车款</p>
+        </header>
+        <div class="main_series">
+            <SeriesImg v-for="(item,index) in seriesDate" :key="index" :item="item"/>
+        </div>
+       <EnlargementImg v-if="EnlargementImgfalg"/>
+       <carAllImg v-if="carAllImg"/>
+    </div>
+</template>
+
+<script>
+
+import {mapActions,mapState} from "vuex"
+import SeriesImg from "@/components/series_img/index.vue"
+import EnlargementImg from "@/components/enlargement_img/index.vue"
+import carAllImg from "@/components/car_all_img/index.vue"
+export default {
+    components:{
+        SeriesImg,
+        EnlargementImg,
+        carAllImg
+    },
+    
+    methods:{
+        ...mapActions({
+            getMasterSeries:'series/getMasterSeries'
+        }),
+        seriescolor(){
+            this.$router.push("/color")
+        },
+        seriestype(){
+            this.$router.push("/type")
+        }
+    },
+    
+    computed:{
+        ...mapState({
+            seriesDate:state=>state.series.seriesDate,
+            EnlargementImgfalg:state=>state.series.EnlargementImgfalg,
+            carAllImg:state=>state.series.carAllImg
+        })
+    },
+    
+    mounted(){
+        let SerialID=this.$route.query.SerialID
+        this.getMasterSeries(SerialID)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+.series_wrap{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background: #eee;
+}
+.series_wrap_header{
+    width: 100%;
+    height: 40px;
+    display: flex;
+    background: #ffffff;
+    p{
+        flex: 5;
+        font-size: 16px;
+        text-align: center;
+        line-height: 40px;
+    }
+}
+.main_series{
+    flex: 1;
+    overflow: auto;
+}
+// .magnify_img{
+//     width: 100%;
+//     height: 100%;
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     background: red;
+// }
+</style>
